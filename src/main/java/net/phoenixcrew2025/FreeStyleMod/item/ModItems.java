588bb10066810e6fdb4363cb42cdf2e2a76bc03d
PhoenixCrew2025 <support@easycreate.org>
2025-06-12 11:52:28 -0400

    Fixed the Badge Name and Added a Tooltip to it

--- src/main/java/net/phoenixcrew2025/FreeStyleMod/item/ModItems.java
+++ src/main/java/net/phoenixcrew2025/FreeStyleMod/item/ModItems.java
@@ -1,10 +1,15 @@
 package net.phoenixcrew2025.FreeStyleMod.item;
 
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import net.phoenixcrew2025.FreeStyleMod.FreeStyleMod;
+
+import java.util.List;
 
 public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(FreeStyleMod.MOD_ID);
@@ -22,7 +27,14 @@ public class ModItems {
     public static final DeferredItem<Item> ONE_THOUSAND_DOLLAR = ITEMS.register("one_thousand_dollar",
             () -> new Item(new Item.Properties()));
     public static final DeferredItem<Item> GOD_KILLER_BADGE = ITEMS.register("god_killer_badge",
-            () -> new Item(new Item.Properties()));
+            () -> new Item(new Item.Properties()) {
+                @Override
+                public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+                    tooltipComponents.add(Component.translatable("tooltip.freestylemod.god_killer_badge.tooltip"));
+                    super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
+                }
+            });
+
 
 
 
